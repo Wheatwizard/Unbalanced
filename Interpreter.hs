fa4901e860d3985000d154a7d4888f1bdf381916
wheatwizard <olivee3@rpi.edu>
2017-07-03 15:17:42 -0400

    Working interpreter

---
+++ Interpreter.hs
@@ -0,0 +1,70 @@
+module Interpreter (interpret) where
+
+inside :: String -> Int -> String
+
+inside ('}':_) 1 = []
+inside ('{':ax) z = '{':(inside ax (z+1))
+inside ('}':ax) z = '}':(inside ax (z-1))
+inside (a:ax) z = a:(inside ax z)
+
+outside :: String -> Int -> String
+
+outside ('}':ax) 1 = ax
+outside ('{':ax) z = '{':(outside ax (z+1))
+outside ('}':ax) z = '}':(outside ax (z-1))
+outside (a:ax) z = a:(outside ax z)
+
+
+f :: String -> ([Integer], [Integer]) -> ([Integer], [Integer])
+
+f [] x = x
+
+f ('(':ax) ((l:lx), rx) = f ax (((l-1):lx), rx)
+f ('(':ax) ([], rx) = f ax ([-1], rx)
+f (')':ax) ((l:lx), rx) = f ax (((l+1):lx), rx)
+f (')':ax) ([], rx) = f ax ([1], rx)
+
+f (']':ax) ((l:lx), (r:rx)) = f ax ((l:r:lx), rx)
+f (']':ax) ([], rx) = f (']':ax) ([0], rx)
+f (']':ax) (lx, []) = f (']':ax) (lx, [0])
+f ('[':ax) ((l:r:lx), rx) = f ax ((l:lx), (r:rx))
+f ('[':ax) ((l:[]), rx) = f ('[':ax) (l:0:[], rx)
+f ('[':ax) ([], rx) = f ('[':ax) ([0,0], rx)
+
+f ('>':ax) (lx, (r:rx)) = f ax ((r:lx), rx)
+f ('>':ax) (lx, []    ) = f ax ((0:lx), [])
+f ('<':ax) ((l:lx), rx) = f ax (lx, (l:rx))
+f ('<':ax) ([],     rx) = f ax ([], (0:rx))
+
+f ('{':ax) (l:lx, rx) = f (outside ax 1) (run (inside ax 1) l (f (inside ax 1) (l:lx, rx)))
+f ('{':ax) ([], rx) = f ('{':ax) ([0], rx)
+
+run :: String -> Integer -> ([Integer], [Integer]) -> ([Integer],[Integer])
+run s t ([], rx) = run s t ([0], rx)
+run s t (l:lx, rx)
+ | t == l = (l:lx, rx)
+ | t /= l = run s t (f s (l:lx, rx))
+
+padding :: String -> (Int, Int) -> (Int, Int)
+
+padding [] x = x
+padding ('{':sx) (a,b) = padding sx (a,b+1)
+padding ('}':sx) (a,0) = padding sx (a+1,0)
+padding ('}':sx) (a,b) = padding sx (a,b-1)
+padding (_:sx)   (a,b) = padding sx (a,b)
+
+pad :: String -> String
+
+pad x = (replicate (fst$padding x (0,0)) '{') ++ x ++ (replicate (snd$padding x (0,0)) '}')
+
+trim :: [Integer] -> [Integer]
+
+trim [] = []
+trim (0:x) = trim x
+trim x
+ | last x == 0 = trim$ init x
+ | otherwise   = x
+
+interpret :: String -> [Integer] -> [Integer]
+
+interpret source input = trim$ (\(a,b) -> (reverse a) ++ b)(f (pad source) ((reverse input), []))
---
+++ Unbalanced.hs
@@ -0,0 +1,69 @@
+import Interpreter
+import System.Environment
+import Data.List
+import Data.Char
+
+version :: String
+version = "Unbalanced 0.0"
+
+isFlag :: String -> Bool
+isFlag ('-':c:x)
+ | elem c "0123456789" = False
+ | otherwise           = True
+isFlag x = False
+
+split :: String -> [String]
+split [] = []
+split (x:xs) = (x:[]):(split xs)
+
+parseFlags :: [String] -> [String] -> [String]
+parseFlags [] a = a
+parseFlags (('-':'-':x):xs) a = parseFlags xs (x:a)
+parseFlags (('-':x):xs)     a = parseFlags xs ((split x) ++ a)
+
+processInput :: [String] -> [Integer]
+processInput (('"':ys):xs)
+ | (last ys) == '"' = map (toInteger.ord) (init ys)
+ | otherwise = error "Invalid input"
+processInput (('\'':ys):xs)
+ | (last ys) == '\'' = map (toInteger.ord) (init ys)
+ | otherwise = error "Invaid input"
+processInput x = [read a :: Integer | a <- x]
+
+helpMenu :: String
+helpMenu = unlines ["",
+  "Unbalanced Haskell Interpreter",
+  "Usage:",
+  "\tUnbalanced [options] source_file args...",
+  "",
+  "   -a, --ascii-out\t\tOutputs as ascii character codes.",
+  "   -e, --execute\t\tExecutes the first commandline argument as Unbalanced code.",
+  "   -h, --help\t\t\tPrints this menu and exits.",
+  "   -v, --version\t\tPrints the version number of the interpreter and exits."]
+
+displayInteger :: Integer -> String
+displayInteger x = show x ++ " "
+
+main :: IO ()
+main = do 
+ cla <- getArgs
+ let flags = parseFlags (filter isFlag cla) []
+ let args  = filter (not.isFlag) cla
+ putStr  (if ((elem "h" flags)||(elem "help" flags)) then (helpMenu++"\n") else [])
+ putStr  (if ((elem "v" flags)||(elem "version" flags)) then (version++"\n") else [])
+ let exiting = any (`elem`["h","help","v","version"]) flags
+ source <- (if exiting then
+             (return "")
+           else
+             (if ((elem "e" flags)||(elem "execute" flags)) then
+               (return (head args))
+             else
+               (readFile (head args))
+             )
+           )
+ let formatOutput = if (elem "a" flags)||(elem "ascii-out" flags) then (\x -> (chr.fromInteger) x:[]) else displayInteger
+ putStr$ (if exiting then
+           ""
+          else
+           concat (map formatOutput (interpret source (processInput (tail args))))
+          )
